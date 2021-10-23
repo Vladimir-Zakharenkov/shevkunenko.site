00274098617acf1bcedb8b75a6ca770bf3387076
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-24 01:02:01 +0300

    Добавляем модель TitleOfPage

---
+++ Site/Models/TitleOfPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Site.Models
+{
+    public class TitleOfPage
+    {
+        public Guid TitleOfPageId { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = "Сайт, посвященный Сергею Шевкуненко";
+        public uint NumberOfPage { get; set; } = 0;
+    }
+}
