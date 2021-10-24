836d651468fbe2487168009f9824dcc8786e39cf
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-24 19:46:30 +0300

    Добавляем FakeTitleOfPageRepository

---
+++ Site/Models/FakeTitleOfPageRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Site.Models
+{
+    public class FakeTitleOfPageRepository : ITitleOfPageRepository
+    {
+        public IQueryable<TitleOfPage> TitleOfPages => new List<TitleOfPage>()
+        {
+            new TitleOfPage
+             {
+                TitleOfPageId = Guid.NewGuid(),
+                NumberOfPage = 0,
+                Title = "Сайт, посвященный Сергею Шевкуненко"
+
+            },
+            new TitleOfPage
+            {
+                TitleOfPageId = Guid.NewGuid(),
+                NumberOfPage = 1,
+                Title = "Сергей Шевкуненко - фильмы, книги, статьи, фото"
+            },
+            new TitleOfPage
+            {
+                TitleOfPageId = Guid.NewGuid(),
+                NumberOfPage = 2,
+                Title = "Шевкуненко Сергей Юрьевич - биография"
+            },
+            new TitleOfPage
+            {
+                TitleOfPageId = Guid.NewGuid(),
+                NumberOfPage = 3,
+                Title = "Фильм «Криминальная звезда» смотреть online"
+            },
+            new TitleOfPage
+            {
+                TitleOfPageId = Guid.NewGuid(),
+                NumberOfPage = 4,
+                Title = "Фильмы с участием Сергея Шевкуненко - смотреть online"
+            }
+        }.AsQueryable<TitleOfPage>();
+    }
+}
