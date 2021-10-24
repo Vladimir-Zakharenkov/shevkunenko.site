b7e8c2914d26622167cd952cbf258b8f5f8912ec
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-24 22:13:59 +0300

    Добаавляем контроллер TitleOfPage

---
+++ Site/Controllers/TitleOfPageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site.Models;
+
+namespace Site.Controllers
+{
+    public class TitleOfPageController : Controller
+    {
+        private ITitleOfPageRepository repository;
+        public TitleOfPageController(ITitleOfPageRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
