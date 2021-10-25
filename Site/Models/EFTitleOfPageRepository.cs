7526b3fd256ed53c183d8fbb0043cd3915496ded
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-25 16:28:52 +0300

    Добавляем класс EFTitleOfPageRepository

---
+++ Site/Models/EFTitleOfPageRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Site.Models
+{
+    public class EFTitleOfPageRepository : ITitleOfPageRepository
+    {
+        private TitleOfPageDbContext context;
+
+        public EFTitleOfPageRepository(TitleOfPageDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<TitleOfPage> TitleOfPages => context.TitleOfPages;
+    }
+}
