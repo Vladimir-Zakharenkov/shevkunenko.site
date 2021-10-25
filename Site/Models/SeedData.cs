3893cefc0e4d407d2f9381bf6a03d42c91f91a3c
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-25 16:48:56 +0300

    Добавляем файл SeedData.cs

---
+++ Site/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System;
+
+namespace Site.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            TitleOfPageDbContext context = app.ApplicationServices.GetRequiredService<TitleOfPageDbContext>();
+
+            if (!context.TitleOfPages.Any())
+            {
+                context.TitleOfPages.AddRange(
+
+                    new TitleOfPage
+                    {
+                        TitleOfPageId = Guid.NewGuid(),
+                        NumberOfPage = 1,
+                        Title = "Сайт, посвященный Сергею Шевкуненко"
+
+                    },
+                    new TitleOfPage
+                    {
+                        TitleOfPageId = Guid.NewGuid(),
+                        NumberOfPage = 2,
+                        Title = "Сергей Шевкуненко - фильмы, книги, статьи, фото"
+                    },
+                    new TitleOfPage
+                    {
+                        TitleOfPageId = Guid.NewGuid(),
+                        NumberOfPage = 3,
+                        Title = "Шевкуненко Сергей Юрьевич - биография"
+                    },
+                    new TitleOfPage
+                    {
+                        TitleOfPageId = Guid.NewGuid(),
+                        NumberOfPage = 4,
+                        Title = "Фильм «Криминальная звезда» смотреть online"
+                    },
+                    new TitleOfPage
+                    {
+                        TitleOfPageId = Guid.NewGuid(),
+                        NumberOfPage = 5,
+                        Title = "Фильмы с участием Сергея Шевкуненко - смотреть online"
+                    }
+
+                    );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
